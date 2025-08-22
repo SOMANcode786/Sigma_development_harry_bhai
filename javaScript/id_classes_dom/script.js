986aa2f6a9bce3f5428b06bd5ff33e30b772d157
Muhammad Soman <somanamir43@gmail.com>
2025-08-22 04:35:00 -0700

    up

---
+++ javaScript/id_classes_dom/script.js
@@ -0,0 +1,15 @@
+console.log("this is the practice course")
+
+
+
+// let name=document.getElementsByClassName("box")
+// console.log("this is the box :" ,name)
+
+// name[2].style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor="green"
+
+
+
+let all=document.querySelectorAll(".box");
+console.log("-->",all)
